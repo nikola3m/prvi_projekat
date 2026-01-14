d672b997d42791ff950965988c7c3f7583ce428c
Nikola Marjanovic <nikola.m.linux@gmail.com>
2026-01-14 23:15:55 +0100

    Refacotr: Uvedena funkcija za ispis zadataka

--- zadaci.py
+++ zadaci.py
@@ -1,21 +1,18 @@
-#Ovo je lista (List)
-poslovi = ["Nauci Git","Nauci Python","Napravi projekat"]
+# DEFINISANJE FUNKCIJA
+def ispisi_listu(lista_zadataka):
+	print("\n--- TVOJI ZADACI ----")
+	for posao in lista_zadataka:
+		print (F"[*]{posao}")
+	print("---------\n")
 
-print("Tvoji trenutni zadaci su:")
-# Ovo je petlja (LOOP) koja prolazi kroz listu
-for posao in poslovi:
-	print(f"- {posao}")
-# Dodajemo novi zadatak preko terminala
-novi_posao= input("Sta jos treba da uradis? ")
-poslovi.append(novi_posao)
+# Glavni deo programa , definisanje liste i poziv funkcije
 
-print(f"Sada imas {len(poslovi)} zadatka: ")
-#Logika: Ako imas puno dazadata, ispisi upozorenje
-if len(poslovi) > 3:
-	print("Vreme je za pauzu, impas previse posla!")
-else:
-	print("Samo napred, mozes jos nesto dodati!")
-#Konacan ispis koda
-print("Konacna lista obaveza: ")
-for posao in poslovi:
-	print(f"-{posao}")
+poslovi = ["Nauci GIT","Ncti Python"]
+
+ispisi_listu(poslovi) #Poziv funkcije da ispise predefinisanu listu
+
+novi = input("Dodaj novi zadatak na listu: ")
+poslovi.append(novi)
+
+ispisi_listu(poslovi) # Drugi poziv funkcije (isti kod, drugi prolaz ispis sa novim zadatkom)
+
